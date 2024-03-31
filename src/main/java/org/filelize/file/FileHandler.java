721886b5f38c784a6b8ae34b553dc629253f3b91
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2024-03-31 10:46:26 +0200

    Now handling NoSuchFileException

--- src/main/java/org/filelize/file/FileHandler.java
+++ src/main/java/org/filelize/file/FileHandler.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,14 +29,22 @@ public class FileHandler {
     }
 
     public <T> T readFile(String fullPath, Class<T> valueType) throws IOException {
-        JsonNode json = objectMapper.readTree(Files.newBufferedReader(Paths.get(fullPath)));
-        return objectMapper.treeToValue(json, valueType);
+        try {
+            JsonNode json = objectMapper.readTree(getNewBufferedReader(fullPath));
+            return objectMapper.treeToValue(json, valueType);
+        } catch (NoSuchFileException e) {
+            return null;
+        }
     }
 
     public <T> Map<String, T> readFileMap(String fullPath, Class<T> valueType) throws IOException {
-        JsonNode json = objectMapper.readTree(Files.newBufferedReader(Paths.get(fullPath)));
-        JavaType mapType = objectMapper.getTypeFactory().constructMapType(Map.class, String.class, valueType);
-        return objectMapper.readValue(json.traverse(), mapType);
+        try {
+            JsonNode json = objectMapper.readTree(getNewBufferedReader(fullPath));
+            JavaType mapType = objectMapper.getTypeFactory().constructMapType(Map.class, String.class, valueType);
+            return objectMapper.readValue(json.traverse(), mapType);
+        } catch (NoSuchFileException e) {
+            return null;
+        }
     }
 
     public <T> Map<String, Object> readFiles(String folderPath, Class<T> valueType) throws IOException {
@@ -56,5 +66,9 @@ public class FileHandler {
     public void writeFile(String fullPath, Object object) throws IOException {
         ensureFile(fullPath);
         objectMapper.writerWithDefaultPrettyPrinter().writeValue(Files.newBufferedWriter(Paths.get(fullPath)), object);
+    }
+
+    private static BufferedReader getNewBufferedReader(String fullPath) throws IOException {
+        return Files.newBufferedReader(Paths.get(fullPath));
     }
 }
---
+++ src/test/java/org/filelize/file/FileHandlerTest.java
@@ -0,0 +1,25 @@
+package org.filelize.file;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.filelize.SomethingElse;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertNull;
+
+public class FileHandlerTest {
+
+    private final FileHandler fileHandler;
+
+    public FileHandlerTest() {
+        this.fileHandler = new FileHandler(new ObjectMapper());
+    }
+
+    @Test
+    public void testReadFile_ShouldHandleNoSuchFileException() throws IOException {
+        var something = fileHandler.readFile("tmp/not_existing_folder.json", SomethingElse.class);
+        assertNull(something);
+    }
+
+}
